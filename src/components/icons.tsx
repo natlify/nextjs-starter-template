8a28e1aac9e7861a7ba7be33a867e88b4f2e0948
Arj Natz <arjith496@gmail.com>
2024-01-15 03:09:29 +0530

    Set up eslint, prettier, husky , lint-staged

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,17 +1,17 @@
 import LinklistLogo from "@/assets/linklist-logo.svg"
-import { Command, Link, LucideProps, Moon, SunMedium } from "lucide-react"
+import { Link, LucideProps, Moon, SunMedium } from "lucide-react"
 
 export type IconKeys = keyof typeof icons
 
 type IconsType = {
-    [key in IconKeys]: React.ElementType
+  [key in IconKeys]: React.ElementType
 }
 
 const icons = {
-    sun: SunMedium,
-    moon: Moon,
-    chainLink: Link,
-    logo: (props: LucideProps) => <LinklistLogo {...props} />,
+  sun: SunMedium,
+  moon: Moon,
+  chainLink: Link,
+  logo: (props: LucideProps) => <LinklistLogo {...props} />,
 }
 
 export const Icons: IconsType = icons
--- src/config/site.ts
+++ src/config/site.ts
@@ -3,16 +3,16 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-    name: "Linklist.cc",
-    author: "natlify",
-    description: "Create lists out of your links. No BS Collation",
-    keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
-    url: {
-        base: env.NEXT_PUBLIC_APP_URL,
-        author: "https://arjnatz.com",
-    },
-    links: {
-        github: "https://github.com/hckrs1/linklist.cc",
-    },
-    ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
+  name: "Linklist.cc",
+  author: "natlify",
+  description: "Create lists out of your links. No BS Collation",
+  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
+  url: {
+    base: env.NEXT_PUBLIC_APP_URL,
+    author: "https://arjnatz.com",
+  },
+  links: {
+    github: "https://github.com/hckrs1/linklist.cc",
+  },
+  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
